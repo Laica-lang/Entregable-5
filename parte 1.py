597ba305d4fe5092cb672160fcdc99e250afcaa5
Jose <lca39524@gmail.com>
2024-12-02 20:15:11 -0500

    commit

---
+++ parte 1.py
@@ -0,0 +1,19 @@
+# Imprimir un mensaje en la consola
+print("Mensaje para la consola")
+
+# Declarar variables de diferentes tipos
+entero = 4
+flotante = 7.3
+cadena = "Hola Mundo"
+
+# Operaciones matemáticas simples
+suma = entero + flotante
+resta = flotante - entero
+multiplicacion = entero * 2
+division = flotante / 2
+
+# Concatenar cadenas de texto
+frase1=input("ingrese la frase 1: ")
+frase2=input("ingrese la frase 2: ")
+frase=(F"{frase1} {frase2}")
+print(frase)
---
+++ parte 2.py
@@ -0,0 +1,17 @@
+# Condicionales
+numero = int(input("Introduce un número: "))
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
+
+# Bucle for
+numeros = [1, 2, 3, 4, 5]
+for n in numeros:
+    print(f"El cuadrado de {n} es {n**2}")
+
+# Bucle while
+entrada = ""
+while entrada != "0":
+    entrada = input("Introduce un número (0 para salir): ")
+    print(f"Has introducido: {entrada}")
